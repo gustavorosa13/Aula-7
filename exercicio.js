5709f6f474c288a1cd9b0cc58f8ecb08dd75772c
Gustavo Gonçalves da Rosa <142434921+gustavorosa13@users.noreply.github.com>
2023-08-23 17:12:05 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,140 @@
+/*
+let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+*/
+
+// idade
+
+/*
+let minhaIdade = 17;
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois, minhaIdade");
+*/
+
+/*
+let numeroUm = 3;
+let numeroDois = 4;                   
+let numeroTres = 5;
+let numeroQuatro = -1;
+let numeroCinco = 2;
+let numeroSeis = 234;
+
+let resultadoDois = 15;
+let resultadoTres = 1;
+
+
+//Resultado feito por mim
+
+//1
+console.log(numeroUm + numeroDois); // 7
+// Resultado 7
+
+
+//2
+console.log(numeroUm * numeroTres)// 15
+console.log(resultadoDois/numeroCinco);// 7.5
+// Resultado 7.5
+
+
+//3
+console.log(numeroTres-numeroDois);// 1
+console.log(resultadoTres*numeroQuatro);// -1
+// Resultado -1
+
+
+//4
+console.log(numeroSeis/numeroTres);// 46.8
+// Resultado 46.8
+*/
+
+
+//Resultado Pedido
+
+
+
+// Exercicio 2
+
+// Comandos: igula: === | diferente: !== | maior e menor igual : > e >= | menor e maior igual:< <=
+
+
+/*
+let numeroUm = 2;
+let numeroDois = 3;
+
+console.log(numeroUm === numeroDois); // False
+
+
+console.log(numeroUm !== numeroDois);// Ture
+
+
+console.log(numeroUm > numeroDois);// False
+
+
+console.log(numeroUm < numeroDois);// True
+*/
+
+/*
+let A = 5;
+let B = 3;
+let C = 0;
+
+console.log(A === B || A !== C);
+console.log(A === B && A !== C);
+console.log(" ---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/ //teste igual ou diferente
+
+
+//Exercício 3
+
+let A = "true";
+let B = "false";
+let C = "true";
+
+/*
+console.log(A && B);
+console.log(B && C);
+console.log(A && C);
+console.log(A && B && C);
+*/ // comando E - &&
+
+/*
+console.log(A || B);
+console.log(B || C);
+console.log(A || C);
+console.log(A || B || C);
+*/ // comando OU - ||
+
+// Operador de negação (!) - Vai trazer o oposto
+/*
+console.log(a);
+console.log(!a);
+
+console.log(b);
+console.log(!b);
+*/
+
+
+// Exercício 5
+
+let nome = "Gustavo"
+let idade = 17
+let maior = idade > 17;
+let ano = 27
+ano = ano + idade
+
+console.log(nome, "Você tem", idade, "anos", "Você é maior de idade?", maior, "em 2050 você terá", ano, "anos");
+
